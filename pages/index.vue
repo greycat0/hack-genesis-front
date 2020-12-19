bc171cda328956c734dc69ae725e6bf9055f648e
Amir <amir@musaev.online>
2020-12-19 23:11:10 +0000

    feat: V1

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,53 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="d-flex">
+    <russian-map
+      class="flex-grow-1"
+      :pickedRegions="pickedRegions"
+      @add="(regionID) => this.pickedRegions.push(regionID)"
+      @delete="
+        (regionID) =>
+          (pickedRegions = pickedRegions.filter((r) => r != regionID))
+      "
+    />
+    <v-card style="max-width: 300px">
+      <v-toolbar flat color="deep-purple accent-4" dark>
+        <v-toolbar-title>Фильтры</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <h2 class="title mb-2">Регионы</h2>
+
+        <v-chip-group column multiple>
+          <v-chip v-for="region in pickedRegions" :key="region" outlined>
+            {{ regions[region] }}
+          </v-chip>
+        </v-chip-group>
+      </v-card-text>
+
+      <v-card-text>
+        <h2 class="title mb-2">Период</h2>
+
+        <v-date-picker v-model="dates" range></v-date-picker>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import Map from '~/components/Map'
+import { regions } from '~/components/regions'
 export default {
   components: {
-    Logo,
-    VuetifyLogo,
+    RussianMap: Map,
+  },
+  data() {
+    return {
+      dates: [],
+      regions,
+      pickedRegions: [],
+    }
   },
 }
 </script>
+
+<style>
+</style>
